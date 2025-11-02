8eafa8568a428b1d9f626da8eaaf072fff9f6f33
Timen Bobnar <61047163+timenbob@users.noreply.github.com>
2025-11-02 13:03:04 +0100

    gg

---
+++ gostejsa_triang.py
@@ -0,0 +1,16 @@
+import trimesh as tm
+from trimesh import remesh
+
+mesh = tm.load_mesh("novi2.stl", force='mesh')
+
+# variant A1: ciljna največja dolžina roba (manjša -> gostejša)
+max_edge = 0.0005  # prilagodi: manjše število => bolj fina mreža
+# remesh zahteva posebej oglišča in ploskve
+vertices, faces = remesh.subdivide_to_size(mesh.vertices, mesh.faces, max_edge=max_edge)
+
+# ustvari novo mrežo
+mesh_dense = tm.Trimesh(vertices=vertices, faces=faces, process=True)
+
+mesh_dense.apply_translation(-mesh_dense.center_mass)  # če rabiš centrirano
+mesh_dense.export("novi3.stl")
+print("Saved input_dense.stl, triangles:", len(mesh_dense.faces))
